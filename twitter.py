6c801937e2d18e7f03898ecdb76a5f9d3ef1f68c
Leonard Richardson <leonardr@segfault.org>
2017-06-18 21:13:12 -0400

    Created what the TwitterPublisher constructor will look like.

--- twitter.py
+++ twitter.py
@@ -3,8 +3,12 @@ import tweepy
 from bot import Publisher
 
 class TwitterPublisher(Publisher):
-    pass
-
-
+    def __init__(
+            self, consumer_key, consumer_secret, access_token,
+            access_token_secret
+    ):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
 
 Publisher = TwitterPublisher
